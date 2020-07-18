ef5d0cc30dca6cd63c77b12636caed0c632db07d
emanuel71jo <emanuel71jo@gmail.com>
2020-07-18 12:52:09 -0300

    Refatoração do código

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 
 const interval = setInterval(() => {
-    const header = document.querySelector('._3auIg');
+    const header = document.querySelector('._1QUKR');
 
     if (header) {
         clearInterval(interval);
 
-        const button = document.createElement('button');
-        button.innerHTML = '2x';
-        button.classList.add('twoTimesButton');
-
-        button.addEventListener('click', () => {
-            const audios = document.querySelectorAll('audio');
-
-            audios.forEach((audio) => {
-                audio.playbackRate = 2;
-            });
-        });
+        const button = createButtonSpeedAudio();
 
         header.appendChild(button);
     }
 }, 1000);
+
+function createButtonSpeedAudio() {
+    const button = document.createElement('button');
+    button.innerHTML = '2x';
+    button.classList.add('twoTimesButton');
+
+    button.addEventListener('click', () => {
+        const audios = document.querySelectorAll('audio');
+
+        audios.forEach((audio) => {
+            if (audio.playbackRate === 2) {
+                audio.playbackRate = 1;
+                button.innerHTML = '2x';
+            } else {
+                button.innerHTML = '1x';
+                audio.playbackRate = 2;
+            }
+        });
+    });
+
+    return button;
+}
